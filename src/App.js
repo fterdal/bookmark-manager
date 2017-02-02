c4e3376e3eb85a72e40438d36ee942248e5b4390
Finn Terdal <fterdal@gmail.com>
2017-02-01 20:05:37 -0800

    Adds date_added to data schema for link resource

--- src/App.js
+++ src/App.js
@@ -10,22 +10,27 @@ class App extends Component {
         {
           title: "How it feels to learn Javascript in 2016",
           url: "https://hackernoon.com/how-it-feels-to-learn-javascript-in-2016-d3a717dd577f#.olyvd43o1",
+          date_added: "2017-02-1",
         },
         {
           title: "Var, Let, or Const",
           url: "https://medium.com/javascript-scene/javascript-es6-var-let-or-const-ba58b8dcde75#.xv9uppvae",
+          date_added: "2017-02-1",
         },
         {
           title: "ES6 Interactive Guide",
           url: "http://stack.formidable.com/es6-interactive-guide/#/",
+          date_added: "2017-02-1",
         },
         {
           title: "Thinking in React",
           url: "https://facebook.github.io/react/docs/thinking-in-react.html",
+          date_added: "2017-02-1",
         },
         {
           title: "What is React?",
           url: "https://www.youtube.com/watch?v=JPT3bFIwJYA",
+          date_added: "2017-02-1",
         }
       ]
     };
@@ -37,6 +42,7 @@ class App extends Component {
           {this.state.resources.map (
             resource => <li>
                           <a href={resource.url}>{resource.title}</a>
+                          <span> | {resource.date_added}</span>
                         </li>
           )}
         </ul>
