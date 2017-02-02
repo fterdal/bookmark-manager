67af2f5300c5f7566a5e852ce8700e00a741b252
Finn Terdal <fterdal@gmail.com>
2017-02-01 19:50:40 -0800

    Creates minimal data model for Link resource

--- src/App.js
+++ src/App.js
@@ -6,12 +6,37 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      resource: "http://hatchoregon.com",
+      resources: [
+        {
+          title: "How it feels to learn Javascript in 2016",
+          url: "https://hackernoon.com/how-it-feels-to-learn-javascript-in-2016-d3a717dd577f#.olyvd43o1",
+        },
+        {
+          title: "Var, Let, or Const",
+          url: "https://medium.com/javascript-scene/javascript-es6-var-let-or-const-ba58b8dcde75#.xv9uppvae",
+        },
+        {
+          title: "ES6 Interactive Guide",
+          url: "http://stack.formidable.com/es6-interactive-guide/#/",
+        },
+        {
+          title: "Thinking in React",
+          url: "https://facebook.github.io/react/docs/thinking-in-react.html",
+        },
+        {
+          title: "What is React?",
+          url: "https://www.youtube.com/watch?v=JPT3bFIwJYA",
+        }
+      ]
     };
   }
   render() {
     return (
-      <a href={this.state.resource}>Best thing ever.</a>
+      <div>
+        {this.state.resources.map (
+          resource => <a href={resource.url}>{resource.title}</a>
+        )}
+      </div>
     );
   }
 }
