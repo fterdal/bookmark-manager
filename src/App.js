8442b0472fd8c046fcab87e8e294e54e70890026
Finn Terdal <fterdal@gmail.com>
2017-02-01 19:15:19 -0800

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return (
+      <a href="http://hatchoregon.com">Best thing ever.</a>
+    );
+  }
+}
+
+export default App;
