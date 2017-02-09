811cfda4a19fcd12883dc09d4cb8dc64a239bda5
Finn Terdal <fterdal@gmail.com>
2017-02-08 20:22:22 -0800

    Adds a button and corresponding event handler to the Subject component. Changes the text of the button.

--- src/components/Subject.js
+++ src/components/Subject.js
@@ -3,15 +3,35 @@ import React, { Component } from 'react';
 export default class Subject extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      isClicked: false,
+    };
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      isClicked: !prevState.isClicked
+    }));
+    setTimeout(() =>
+      console.log('isClicked: ' + this.state.isClicked), 250);
   }
 
   // Event Handlers
 
   render() {
+    let buttonText = null;
+    if (this.state.isClicked == true) {
+      buttonText = 'Elephant'; }
+    else {
+      buttonText = 'Monkey'; }
     return (
       <div>
-        <h2>{this.props.items.subject}</h2>
+        <h3>{this.props.items.subject}</h3>
+        <button className="btn btn-primary"
+                onClick={() => this.handleClick()}
+                style={this.state.headerColor}>
+          {buttonText}
+        </button>
         <ul className="list-group">
           {this.props.items.resources.map((resource) => {
               return(
