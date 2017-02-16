77fe4aff94bace3bbaff06e7de686921385e97e6
Finn Terdal <fterdal@gmail.com>
2017-02-15 19:59:13 -0800

    Adds two input fields above the subject resources, along with event handler for typing.

--- src/components/Subject.js
+++ src/components/Subject.js
@@ -3,8 +3,16 @@ import React, { Component } from 'react';
 export default class Subject extends Component {
   constructor(props) {
     super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+    this.handleTyping = this.handleTyping.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
     this.state = {
       isClicked: false,
+      inputText: 'Type Here',
+      title: '',
+      url: '',
     };
   }
 
@@ -12,8 +20,19 @@ export default class Subject extends Component {
     this.setState(prevState => ({
       isClicked: !prevState.isClicked
     }));
-    // setTimeout(() =>
-    //   console.log('isClicked: ' + this.state.isClicked), 250);
+  }
+
+  handleTyping(e) {
+    this.setState({[e.target.name]: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const newResource = {
+      title: this.state.title,
+      url: this.state.url,
+    }
+    this.props.addResource(this.props.index, newResource);
   }
 
   render() {
@@ -37,17 +56,24 @@ export default class Subject extends Component {
           </ul>;
     }
     else {
-      listContent = ''; }
+      listContent = null;
+    }
 
     return (
       <div>
         <div>
         <h3>{this.props.items.subject}</h3>
+        <br />
+        <label htmlFor="title">Title</label>
+        <input name="title" onChange={this.handleTyping.bind(this)} value={this.state.title} />
+        <br />
+        <label htmlFor="url">URL</label>
+        <input name="url" onChange={this.handleTyping.bind(this)} value={this.state.url} />
+        </div>
         <button className="btn btn-primary"
                 onClick={() => this.handleClick()}>
           {buttonText}
         </button>
-        </div>
         {listContent}
       </div>
     )
